cc415fa24e78b8d9aad6e147b48b6484dbcbd245
volodymyr-v-konoval <volodymyr.v.konoval@gmail.com>
2025-03-19 23:12:48 +0200

    security upgraded

--- simple_blog/simple_blog/settings.py
+++ simple_blog/simple_blog/settings.py
@@ -59,6 +59,8 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django.middleware.security.SecurityMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
 ]
 
 ROOT_URLCONF = "simple_blog.urls"
