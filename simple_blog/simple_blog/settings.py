fc1060fb0dda2a3d19872ce69528805c951ecc71
volodymyr-v-konoval <volodymyr.v.konoval@gmail.com>
2025-03-24 22:10:24 +0200

    to deploy 2210

--- simple_blog/simple_blog/settings.py
+++ simple_blog/simple_blog/settings.py
@@ -57,6 +57,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -267,3 +268,10 @@ LOGGING = {
         },
     },
 }
+
+# Static files
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+# Whitenoise for file manipulating in production
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
